14089681d5a7b6e5b9453f7eba677fd38116a019
Quame Jnr <kaboateng14@gmail.com>
2021-04-26 02:07:12 +0000

    Installed django-heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,6 @@ STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 # Testing mail
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
