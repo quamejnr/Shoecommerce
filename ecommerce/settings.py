8dc408e84b70e3e761517c4e35292201bf2e76d3
Quame Jnr <kaboateng14@gmail.com>
2021-04-26 05:33:08 +0000

    Corrected a typo in .env file and set debug value

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['shoemall.herokuapp.com']
 
