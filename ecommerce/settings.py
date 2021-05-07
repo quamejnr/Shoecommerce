bff0b30bdf41a649b5620fb122286e32884fc7c8
Quame Jnr <kaboateng14@gmail.com>
2021-05-07 05:21:41 +0000

    Updated settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,7 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+
 ALLOWED_HOSTS = ['shoemall.herokuapp.com']
 
 
