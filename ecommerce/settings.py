ec56bc0fd5b7af6c8742dced8b343cb10091553e
Quame Jnr <kaboateng14@gmail.com>
2021-05-12 08:41:10 +0000

    Made changes to stripe form to make it responsive

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,7 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+
 ALLOWED_HOSTS = ['shoemall.herokuapp.com']
 
 
@@ -144,7 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/shoecommerce/'
 
 # Stripe
-STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
+STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 
 # Testing mail
 EMAIL_HOST = "localhost"
